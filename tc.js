9dadd03ae21817447af00ef9cf436a17feff0d5d
cordis-dev <darius@adroiti.com>
2025-05-31 22:01:36 +0300

    Create tc.js

---
+++ tc.js
@@ -0,0 +1,74 @@
+var ifElseTest = function(text) {
+	if (text === "1") {
+		return text;
+	} else if (text === "2") {
+		return text;
+	} else if (text === "3") {
+		return text;
+	} else if (text === "4") {
+		return text;
+	} else if (text === "5") {
+		return text;
+	} else if (text === "6") {
+		return text;
+	} else if (text === "7") {
+		return text;
+	} else if (text === "8") {
+		return text;
+	} else if (text === "9") {
+		return text;
+	} else if (text === "10") {
+		return text;
+	} else if (text === "11") {
+		return text;
+	} else if (text === "12") {
+		return text;
+	} else if (text === "13") {
+		return text;
+	} else if (text === "14") {
+		return text;
+	} else if (text === "15") {
+		return text;
+	} else {
+		return text;
+	}
+};
+
+var switchTest = function(text) {
+    switch (text) {
+    case "1":
+        return text;
+    case "2":
+        return text;
+    case "3":
+        return text;
+    case "4":
+        return text;
+    case "5":
+        return text;
+    case "6":
+        return text;
+    case "7":
+        return text;
+    case "8":
+        return text;
+    case "9":
+        return text;
+	case "10":
+		return text;
+	case "11":
+		return text;
+	case "12":
+		return text;
+	case "13":
+		return text;
+	case "14":
+		return text;
+	case "15":
+		return text;
+    default:
+        return text;
+    }
+}
+console.log(ifElseTest)
+console.log(switchTest)
